ea0597049585ac187868d6d4255c29d13c11ce86
FrankieFabuloso <frocha@ucsc.edu>
2017-05-07 13:47:50 -0700

    adds sign-up form

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
   $('#log_in_button').click( function() {
     $(this).addClass('invisible')
+    $('#sign_up_button').addClass('invisible')
     $('.form.form-body--log-in').removeClass('invisible')
   })
 
-  $('#landing_cancel_button').click( function() {
+  $('#sign_up_button').click( function() {
+    $(this).addClass('invisible')
+    $('#log_in_button').addClass('invisible')
+    $('.form.form-body--sign-up').removeClass('invisible')
+  })
+
+  $('#log_in_cancel_button').click( function() {
     $('.form.form-body--log-in').addClass('invisible')
     $('#log_in_button').removeClass('invisible')
+    $('#sign_up_button').removeClass('invisible')
+  })
+
+  $('#sign_up_cancel_button').click( function() {
+    $('.form.form-body--sign-up').addClass('invisible')
+    $('#log_in_button').removeClass('invisible')
+    $('#sign_up_button').removeClass('invisible')
   })
 })
